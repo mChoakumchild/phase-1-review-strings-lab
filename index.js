5e265764aafe9a50aae5d7db2f3a431102a50ffd
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-05 10:55:33 -0400

    commit tree f03cb40c4f166317ec94e2f7651fb33ccd9121c6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'; 
+const firstInitial = currentUser.slice(0,1);
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${firstInitial}!`;
